931f98f1df5e250dfda8dd6202a796ca0580514a
MadjerFin <madjer.finamor@gmail.com>
2025-04-01 13:09:21 -0300

    mudança nas respostas do bot

--- duvidas.py
+++ duvidas.py
@@ -7,28 +7,35 @@ client = anthropic.Anthropic(
     # defaults to os.environ.get("")
     api_key=os.environ.get("ANTHROPIC_API_KEY")
 )
-modelo = "claude-3-5-sonnet-20240620"
+modelo = "claude-3-7-sonnet-20250219"
 
-def bot(propmt):
-
-    propmt_de_sistema = "Responda a dúvida do abaixo utilizando informações sobre a linha 8 e 9 do metrô de São Paulo  " \
+def bot(prompt):
+    prompt_de_sistema = (
+        "Responda a dúvida do usuário utilizando informações sobre a linha 8 e 9 do metrô de São Paulo. "
+        "Você não pode responder perguntas que não envolvam o sistema de transporte público de São Paulo. "
+        "Responda com no máximo 2 parágrafos e 200 tokens. "
+        "Priorize respostas curtas e diretas. "
+        "O preço atual do bilhete é de R$ 5,20. Responda isso apenas se for perguntado. "
+        "Você deve responder a pergunta no idioma em que foi perguntado."
+        "Caso a pergunta seja sobre bilhetes redirecione para o site do top e mande um link para o acesso ao whatsapp do top tambem"
+        "Quando a pergunta for sobre acessibilidade, responda com esse site https://mobilidade.grupoccr.com.br/viamobilidade8e9/mapas/#v1-0d6ebb00ed-item-b4ce45656a-tab"
+        "A sé não tem transferencia para a linha 4 amarela"
+    )
     
-    
-
-    propmt_do_usuario = propmt
+    prompt_do_usuario = prompt
 
     message = client.messages.create(
-        model= modelo,
+        model=modelo,
         max_tokens=1000,
         temperature=1,
-        system= propmt_de_sistema,
+        system=prompt_de_sistema,
         messages=[
             {
                 "role": "user",
                 "content": [
                     {
                         "type": "text",
-                        "text": propmt_do_usuario
+                        "text": prompt_do_usuario
                     }
                 ]
             }
